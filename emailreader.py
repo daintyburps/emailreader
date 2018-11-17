78b6dc1a6f6206335d385745f43479aa5e4e29be
carl <thunman@gmail.com>
2018-11-17 17:11:20 -0500

    lint

--- emailreader.py
+++ emailreader.py
@@ -9,6 +9,7 @@ def process_ul(ul_element):
         output[key] = text[0]
     return output
 
+
 def main():
     magic = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
             "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" [
