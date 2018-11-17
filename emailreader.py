dc1d222ba4a669a988467c34398a84a88d07d629
carl <thunman@gmail.com>
2018-11-17 16:51:12 -0500

    prototype up and running

--- emailreader.py
+++ emailreader.py
@@ -1,17 +1,26 @@
 import xml.etree.ElementTree as ET
 
-f = open('testsample.html')
 
-contents = f.read()
+def process_ul(ul_element):
+    output = {}
+    for element in ul_element.iter('li'):
+        key = element[0].text
+        text = [child.tail.strip() for child in element]
+        output[key] = text[0]
 
-tree = ET.fromstring(contents)
-print(len(tree))
-for child in tree:
-	print(child)
-	print(child.text)
-	for c in child:
-		print(c.text)
+    return output
 
-# print(tree.)
-# print(root)
-# print(parsed_html)
+
+def main():
+    magic = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+            "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" [
+            <!ENTITY nbsp ' '>
+            ]>'''
+    f = open('testsample.html')
+    contents = f.read()
+
+    root = ET.XML(magic + contents)
+    print(process_ul(root))
+
+if __name__ == '__main__':
+    main()
